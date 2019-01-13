fe5cdd3b10b9446f5ee32bcb1ab801227783017e
makidelille <makidelille@hotmail.fr>
2019-01-13 18:06:34 +0100

    chrome extension +

--- chrome/contentscript.js
+++ chrome/contentscript.js
@@ -1,10 +1,25 @@
-console.log("hello");
+console.log("hello you are on *.dejbox.fr");
 
+var urlFinish = "test";
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      // listen for messages sent from background.js
-      if (request.message === 'hello!') {
-        console.log(request.url) // new url is now in content scripts!
-      }
+if(window.location.href.indexOf(urlFinish) != -1){
+  // we are on the final page.
+  var idSpan = document.querySelector('body').innerHTML;
+
+  chrome.storage.sync.get(function(val){
+    var host = val.url || "http://localhost/";
+    var name =  val.name || "";
+
+    if(host.indexOf('http') != 0){
+      host = 'http://' + host;
+    }
+
+    const req = new XMLHttpRequest();
+    req.onloadend = function(){
+      alert("Dejbox envoyé au serveur ! Bon courage pour la suite");
+    }
+    req.open('POST', host, false); 
+    req.send(JSON.stringify({id: idSpan, name: name}));
+    
   });
+}
--- chrome/options.js
+++ chrome/options.js
@@ -1,15 +1,22 @@
-let page = document.getElementById('buttonDiv');
-const kButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
-function constructOptions(kButtonColors) {
-  for (let item of kButtonColors) {
-    let button = document.createElement('button');
-    button.style.backgroundColor = item;
-    button.addEventListener('click', function() {
-      chrome.storage.sync.set({color: item}, function() {
-        console.log('color is ' + item);
-      })
-    });
-    page.appendChild(button);
-  }
-}
-constructOptions(kButtonColors);
+let inputUrl = document.querySelector("input[name='url']");
+let inputName = document.querySelector("input[name='name']");
+
+inputName.addEventListener('change', function(){
+  var val = this.value;
+  chrome.storage.sync.set({name: val}, function(){
+    console.log("this.value: " +  val);
+  })
+});
+
+inputUrl.addEventListener('change', function(){
+  var val = this.value;
+  chrome.storage.sync.set({url: val}, function(){
+    console.log("this.value: " +  val);
+  })
+});
+
+
+chrome.storage.sync.get(function(val){
+  inputUrl.value = val.url || "";
+  inputName.value = val.name || "";
+})
