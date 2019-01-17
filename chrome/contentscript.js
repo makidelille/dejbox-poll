3bf5b6b63749d86d6a672f7d2a11f0237d9664ee
Julien DONQUE <Julien.DONQUE@numen.fr>
2019-01-17 09:42:26 +0100

    ajout du plugin chrome

--- chrome/contentscript.js
+++ chrome/contentscript.js
@@ -1,25 +1,27 @@
-console.log("hello you are on *.dejbox.fr");
+console.log("hello you are on dejbox.fr/order_confirmation.jsp");
 
-var urlFinish = "test";
+"https://www.dejbox.fr/order_confirmation.jsp?order=1986357";
+"https://www.dejbox.fr/order_confirmation.jsp?order=2004833";
 
-if(window.location.href.indexOf(urlFinish) != -1){
   // we are on the final page.
-  var idSpan = document.querySelector('body').innerHTML;
+var idSpan = document.querySelector('.order_number').innerHTML;
 
-  chrome.storage.sync.get(function(val){
-    var host = val.url || "http://localhost/";
-    var name =  val.name || "";
+chrome.storage.sync.get(function(val){
+  var host = val.url || "http://localhost/";
+  var name =  val.name || "";
 
-    if(host.indexOf('http') != 0){
-      host = 'http://' + host;
-    }
+  if(host.indexOf('http') != 0){
+    host = 'http://' + host;
+  }
 
-    const req = new XMLHttpRequest();
-    req.onloadend = function(){
-      alert("Dejbox envoyé au serveur ! Bon courage pour la suite");
-    }
-    req.open('POST', host, false); 
-    req.send(JSON.stringify({id: idSpan, name: name}));
-    
-  });
-}
+
+  console.log('Enregistrement de la commande n°' + idSpan);
+  const req = new XMLHttpRequest();
+  req.onloadend = function(){
+    alert("Dejbox envoyÃ© au serveur ! Bon courage pour la suite");
+  }
+  req.open('POST', host, false); 
+  req.setRequestHeader("Content-Type", "application/json");
+  req.send(JSON.stringify({dejbox: idSpan, name: name}));
+
+});
