1fb7d33de9b874913ed542c56306e40e54e69f47
makidelille <makidelille@hotmail.fr>
2019-01-17 22:05:50 +0100

    options auto à l'install

---
+++ BrowerExtension/dejbox-poll/background.js
@@ -0,0 +1,10 @@
+chrome.runtime.onInstalled.addListener(function(details){
+    if(details.reason == "install"){
+        console.log("This is a first install!");
+    }else if(details.reason == "update"){
+        var thisVersion = chrome.runtime.getManifest().version;
+        console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+    }
+
+    chrome.runtime.openOptionsPage()
+});
